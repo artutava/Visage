eb7bd9ef0d2e20af291cb790bbe9326f7505faf4
Artur Tavares <artutava@hotmail.com>
2023-05-07 11:27:34 -0400

    Revert "List scrollable in addon"
    This reverts commit 72e0ef8b50ef0a48c7aa1416bde5f7220a1aec07.

--- __init__.py
+++ __init__.py
@@ -5,23 +5,8 @@ bl_info = {
 }
 
 import bpy
-from bpy.props import StringProperty, IntProperty
-from bpy.types import Panel, UIList, Operator
 
-class OBJECT_UL_CustomShapeKeyList(UIList):
-    def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
-        shape_key = item
-        if shape_key.name.startswith("FC_"):
-            layout.prop(shape_key, "name", text="", emboss=False)
-            layout.prop(shape_key, "value", text="")
-            layout.prop(shape_key, "mute", text="", icon="LOCKED" if shape_key.mute else "UNLOCKED")
-            icon = "PINNED" if context.object.show_only_shape_key else "UNPINNED"
-            op = layout.operator("object.pin_shape_key", text="", icon=icon)
-            op.index = data.key_blocks.find(shape_key.name)
-            op = layout.operator("object.shape_key_remove", text="", icon="X")
-            op.index = data.key_blocks.find(shape_key.name)
-
-class OBJECT_PT_CustomShapeKeysPanel(Panel):
+class OBJECT_PT_CustomShapeKeysPanel(bpy.types.Panel):
     bl_label = "FC Shape Keys"
     bl_idname = "OBJECT_PT_custom_shape_keys"
     bl_space_type = 'VIEW_3D'
@@ -43,15 +28,26 @@ class OBJECT_PT_CustomShapeKeysPanel(Panel):
         row = layout.row()
         row.operator("object.shape_key_add", text="Add Shape Key").from_mix = False
 
-        # Search bar
-        row = layout.row()
-        row.prop(obj, "custom_shape_key_filter", text="", icon="VIEWZOOM")
+        for shape_key in shape_keys:
+            if shape_key.name.startswith("FC_"):
+                row = layout.row(align=True)
 
-        # Custom shape key list
-        box = layout.box()
-        row = box.row()
-        row.template_list("OBJECT_UL_CustomShapeKeyList", "", shape_key_data, "key_blocks",
-                          obj, "custom_shape_key_list_index", rows=4)
+                # Shape key name
+                row.prop(shape_key, "name", text="")
+                # Shape key value slider
+                row.prop(shape_key, "value", text="")
+                # Shape key mute (lock) toggle
+                row.prop(shape_key, "mute", text="", icon="LOCKED" if shape_key.mute else "UNLOCKED")
+                # Edit mode toggle
+                if obj.mode == 'EDIT' and obj.active_shape_key == shape_key:
+                    row.operator("object.editmode_toggle", text="", icon="SHAPEKEY_DATA")
+                else:
+                    op = row.operator("object.editmode_toggle", text="", icon="EDITMODE_HLT")
+                    op.index = shape_key_data.key_blocks.find(shape_key.name)
+
+                # Remove shape key button
+                op = row.operator("object.shape_key_remove", text="", icon="X")
+                op.index = shape_key_data.key_blocks.find(shape_key.name)
 
         # Other shape key options
         if shape_key_data.use_relative:
@@ -60,51 +56,48 @@ class OBJECT_PT_CustomShapeKeysPanel(Panel):
             layout.prop(shape_key_data, "slider_min", text="Range Min")
             layout.prop(shape_key_data, "slider_max", text="Range Max")
 
-class OBJECT_OT_PinShapeKey(Operator):
-    bl_idname = "object.pin_shape_key"
-    bl_label = "Pin Shape Key"
+class OBJECT_OT_EditModeToggle(bpy.types.Operator):
+    bl_idname = "object.editmode_toggle"
+    bl_label = "Toggle Edit Mode"
     bl_options = {'REGISTER', 'UNDO'}
 
-    index: IntProperty()
+    index: bpy.props.IntProperty()
 
     def execute(self, context):
-        active_object = bpy.context.active_object
-        if active_object.type == 'MESH' and active_object.data.shape_keys:
-            active_object.show_only_shape_key = not active_object.show_only_shape_key
+        obj = context.object
+        shape_key_data = obj.data.shape_keys
+
+        if obj.mode == 'EDIT':
+            bpy.ops.object.editmode_toggle()
+        else:
+            obj.active_shape_key_index = self.index
+            bpy.ops.object.editmode_toggle()
+
         return {'FINISHED'}
 
-class OBJECT_OT_ShapeKeyRemove(Operator):
+class OBJECT_OT_ShapeKeyRemove(bpy.types.Operator):
     bl_idname = "object.shape_key_remove"
     bl_label = "Remove Shape Key"
     bl_options = {'REGISTER', 'UNDO'}
 
-    index: IntProperty()
+    index: bpy.props.IntProperty()
 
     def execute(self, context):
         obj = context.object
         shape_key_data = obj.data.shape_keys
         shape_key_data.key_blocks.remove(shape_key_data.key_blocks[self.index])
+
         return {'FINISHED'}
-    
+
 def register():
-    bpy.utils.register_class(OBJECT_UL_CustomShapeKeyList)
     bpy.utils.register_class(OBJECT_PT_CustomShapeKeysPanel)
-    bpy.utils.register_class(OBJECT_OT_PinShapeKey)
+    bpy.utils.register_class(OBJECT_OT_EditModeToggle)
     bpy.utils.register_class(OBJECT_OT_ShapeKeyRemove)
-    bpy.types.Object.custom_shape_key_list_index = IntProperty()
-    bpy.types.Object.custom_shape_key_filter = StringProperty()
 
 def unregister():
     bpy.utils.unregister_class(OBJECT_OT_ShapeKeyRemove)
-    bpy.utils.unregister_class(OBJECT_OT_PinShapeKey)
+    bpy.utils.unregister_class(OBJECT_OT_EditModeToggle)
     bpy.utils.unregister_class(OBJECT_PT_CustomShapeKeysPanel)
-    bpy.utils.unregister_class(OBJECT_UL_CustomShapeKeyList)
-    del bpy.types.Object.custom_shape_key_list_index
-    del bpy.types.Object.custom_shape_key_filter
 
 if name == "main":
     register()
-
-
-
-
